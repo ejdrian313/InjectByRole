ca5b9ef7ae1f9beea1c19892feaab7cf4c5be49d
Adrian Kujawski <akujawski@mdevelopers.com>
2021-04-04 21:27:34 +0200

    add list extension helper

---
+++ InjectByRole/Helpers/IListExt.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using LanguageExt;
+
+namespace InjectByRole.Handlers
+{
+    public static class IListExt
+    {
+        public static bool IsNullOrEmpty<T>(this IList<T> list)
+        {
+            return list.IsNull() || !list.Any();
+        }
+    }
+}
